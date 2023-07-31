02568a29315ee323cb8c4fe1ac5531a4cde7ede7
Arunkumar00712 <earun142@gmail.com>
2023-07-31 15:51:37 +0530

    first commit message

---
+++ src/com/data/Students.java
@@ -0,0 +1,26 @@
+package com.data;
+
+public class Students {
+	private void StudentsDetails(String name) {
+	
+	System.out.println();
+	}
+	
+	public static void main(String[] args) {
+	
+	Students e=new Students();
+	e.StudentsDetails("arun");
+		
+	
+		
+	System.out.println();	
+	}
+	
+
+	
+	
+	
+	
+	}	
+	
+
